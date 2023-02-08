0faccc92c481517df66f66baf42c549856cc0a6d
Spasova <eexemplega@gmail.com>
2023-02-08 09:07:27 +0200

    layout changed

--- components/Footer.js
+++ components/Footer.js
@@ -12,31 +12,25 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-  const { ref: cardRef, inView: IsIconVisible } = useInView({threshold:0.2});
+  const { ref: cardRef, inView: IsIconVisible } = useInView({threshold:0.5});
 
   return (
     <div className={`${styles["footer-container"]}`} >
-      <div className={`${styles["footer-sm-icons"]}`} >
+      <div className={`${styles["footer-sm-icons"]} 
+      ${IsIconVisible ? `${styles["iconAnim"]}` : ""}`}
+        ref={cardRef} >
        <FontAwesomeIcon
         icon={faYoutube} 
-        className={`${styles["iconYoutube"]}
-        ${IsIconVisible ? `${styles["iconAnim"]}` : ""}`}
-        ref={cardRef} />
+        className={`${styles["iconYoutube"]}`}/>
        <FontAwesomeIcon
         icon={faLinkedin}
-        className={`${styles["iconLinkedin"]}
-        ${IsIconVisible ? `${styles["iconAnim"]}` : ""}`}
-        ref={cardRef} />              
+        className={`${styles["iconLinkedin"]}`}/>              
        <FontAwesomeIcon
         icon={faGithub}
-        className={`${styles["iconGithub"]}
-        ${IsIconVisible ? `${styles["iconAnim"]}` : ""}`}
-        ref={cardRef} />
+        className={`${styles["iconGithub"]}`}/>
        <FontAwesomeIcon
         icon={faDev}
-        className={`${styles["iconDev"]}
-        ${IsIconVisible ? `${styles["iconAnim"]}` : ""}`}
-        ref={cardRef} />  
+        className={`${styles["iconDev"]}`}/>  
       </div> 
       <div className={`${styles["footer-info"]}`} >
       
