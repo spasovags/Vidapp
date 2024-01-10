8a83869a9214f08497aad06a51f9de0121177e89
spasovags <eexemplega@gmail.com>
2024-01-10 16:05:05 +0200

    nav butt

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,25 +2,13 @@ import styles from '../styles/Navbar.module.css'
 import { useState, useEffect } from "react";
 import Link from 'next/link'
 import Image from 'next/image'
-import logo from '../public/favicon.png'
+import logo from '../public/favicon.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 const Navbar = () => {
-  
-    const [isModal, setIsModal] = useState(false);
-    const contentClassname = isModal
-        ? `${styles["hideMenu"]} ${styles["nav-links-container"]}`
-        : `${styles["nav-links-container"]}`;
-
-    const [isActive, setIsActive] = useState(false);
-    const hambClassname = isActive
-            ? `${styles["active"]} ${styles["menu-container"]} ${styles["icon"]}`
-            : `${styles["menu-container"]} ${styles["icon"]}`;
-    
-   
-
+      
   return (
     <nav className={`${styles["nav-background"]}`}>
       <div className={`${styles["navbar-container"]}`}>
@@ -28,22 +16,14 @@ const Navbar = () => {
           <Link href="#">
             <Image src={logo} height={45} width={60} alt="Link home" />
           </Link>
-          <div href="#" className={hambClassname} 
-          onClick={() => {setIsModal(!isModal); setIsActive(!isActive)}}>
+          <div className={`${styles["following-foryou-container"]}`}>
+            <button>Following</button>
+            <button>For You</button>
+          </div>
+          <div className={`${styles["search-container"]}`}>
             <FontAwesomeIcon icon={faMagnifyingGlass} />
           </div>
         </div>
-        <ul className={contentClassname}>
-          <li className={`${styles["menu-li"]}`}>
-            <Link href="#" className={`${styles["menu-link"]}`}>account</Link>
-         </li>
-          <li className={`${styles["menu-li"]}`}>
-            <Link href="#" className={`${styles["menu-link"]}`}>settings</Link>
-            </li>
-          <li className={`${styles["menu-li"]}`}>
-            <Link href="#" className={`${styles["menu-link"]}`}>contact us</Link>
-          </li>
-        </ul>
       </div>
     </nav>
   )
