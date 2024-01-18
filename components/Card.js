e876248a01f5f6873a6668ef5a5808b4ee91902a
spasovags <eexemplega@gmail.com>
2024-01-18 16:02:15 +0200

    icons added

--- components/Card.js
+++ components/Card.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/Card.module.css'
 import { useInView } from 'react-intersection-observer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
 
 const Card = (props) => {
   const post = props.value;
@@ -28,15 +30,25 @@ const Card = (props) => {
             <p>profpic</p>
           </div>
           <div className={`${styles["like"]}`} >
+            <span class="material-symbols-outlined">
+              favorite
+              </span>
             <p>5688</p>
           </div>
           <div className={`${styles["comment"]}`} >
+          <FontAwesomeIcon icon={faCommentDots} />
           <p>8900</p>
           </div>
           <div className={`${styles["save"]}`} >
+            <span class="material-symbols-outlined">
+              bookmark
+              </span>
             <p>9987</p>
           </div>
           <div className={`${styles["share"]}`} >
+            <span class="material-symbols-outlined">
+              switch_access_shortcut
+              </span>
             <p>77</p>
           </div>
           <div className={`${styles["sound"]}`} >
